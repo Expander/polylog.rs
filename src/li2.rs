bab5dc69d1ec2b547e9171bedf96205233fecbd6
Alexander Voigt <ahgvoigt@gmail.com>
2020-05-28 18:18:20 +0200

    5-10% performance improvement of complex dilogarithm
    by using Estrin's scheme

--- src/li2.rs
+++ src/li2.rs
@@ -148,20 +148,18 @@ impl Li2<Complex<f64>> for Complex<f64> {
             }
         };
 
-        // the dilogarithm
         let cz2 = cz*cz;
+        let cz4 = cz2*cz2;
         let sum =
             cz +
             cz2 * (bf[0] +
             cz  * (bf[1] +
-            cz2 * (bf[2] +
-            cz2 * (bf[3] +
-            cz2 * (bf[4] +
-            cz2 * (bf[5] +
-            cz2 * (bf[6] +
-            cz2 * (bf[7] +
-            cz2 * (bf[8] +
-            cz2 * (bf[9]))))))))));
+            cz2 * (
+                bf[2] +
+                cz2*bf[3] +
+                cz4*(bf[4] + cz2*bf[5]) +
+                cz4*cz4*(bf[6] + cz2*bf[7] + cz4*(bf[8] + cz2*bf[9]))
+            )));
 
         sgn * sum + cy
     }
