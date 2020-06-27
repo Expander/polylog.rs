5e968207d33d686844f7a485aced9696e20a3e54
Alexander Voigt <ahgvoigt@gmail.com>
2020-06-28 01:42:43 +0200

    extend comments

--- src/cln.rs
+++ src/cln.rs
@@ -1,6 +1,8 @@
 use num::complex::Complex;
 
-/// Provides an implementation of the complex logarithm `cln()` of a number of type `T`.
+/// Provides an implementation of the complex logarithm `cln()` of a
+/// number of type `T`, where the imaginary part of the logarithm is
+/// within (-Pi,Pi].
 pub trait CLn<T> {
     fn cln(&self) -> T;
 }
@@ -8,7 +10,8 @@ pub trait CLn<T> {
 impl CLn<Complex<f64>> for Complex<f64> {
     fn cln(&self) -> Complex<f64> {
         let z = Complex::new(
-            if self.re == 0. { 0. } else { self.re },
+            self.re,
+            // convert -0.0 to 0.0
             if self.im == 0. { 0. } else { self.im },
         );
         Complex::new(0.5*z.norm_sqr().ln(), z.arg())
