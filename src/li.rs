d83d4de439551bd2cb2788b34b7dbfb09373b5a7
Alexander Voigt <ahgvoigt@gmail.com>
2022-02-25 18:08:51 +0100

    simplify branches

--- src/li.rs
+++ src/li.rs
@@ -45,12 +45,8 @@ impl Li<f64> for f64 {
                 li_series_naive(n, x)
             } else if nl < 0.512*0.512*fp {
                 li_unity_neg(n, x)
-            } else if x.abs() <= 0.75 {
-                li_series_naive(n, x)
-            } else if x.abs() > 1.4 {
-                odd_sgn(n)*li_series_naive(n, x.recip())
             } else {
-                panic!("panic: n = {}, x = {}", n, x);
+                odd_sgn(n)*li_series_naive(n, x.recip())
             }
         } else if n == -1 {
             *self/((1.0 - *self)*(1.0 - *self))
