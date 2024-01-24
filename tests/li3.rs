115e72ccac7738861a527fd887bc75a8ac05fe86
Alexander Voigt <ahgvoigt@gmail.com>
2024-01-24 06:42:01 +0100

    use explicit f64 type instead of {float}

--- tests/li3.rs
+++ tests/li3.rs
@@ -1,34 +1,34 @@
 use num::complex::Complex;
-use num::Float;
 use polylog::Li3;
 mod common;
 use common::CLn;
 
 
 fn id1(z: Complex<f64>) -> Complex<f64> {
-    z.li3() + (-z).li3() - 0.25*(z*z).li3()
+    z.li3() + (-z).li3() - 0.25_f64*(z*z).li3()
 }
 
 
 fn id2(z: Complex<f64>) -> Complex<f64> {
-    if z.norm() < std::f64::EPSILON || (z.re > 0. && z.re < 1.) {
-        Complex::new(0.,0.)
+    if z.norm() < std::f64::EPSILON || (z.re > 0.0_f64 && z.re < 1.0_f64) {
+        Complex::new(0.0_f64, 0.0_f64)
     } else {
         let pi = std::f64::consts::PI;
-        z.li3() - (1./z).li3() + (-z).cln().powi(3)/6. + pi*pi/6.*(-z).cln()
+        z.li3() - (1.0_f64/z).li3() + (-z).cln().powi(3)/6.0_f64 + pi*pi/6.0_f64*(-z).cln()
     }
 }
 
 
 fn id3(z: Complex<f64>) -> Complex<f64> {
-    if (1.0 - z).re.abs() < std::f64::EPSILON || (z.re <= 0. && z.im == 0.) {
-        Complex::new(0.,0.)
+    if (1.0_f64 - z).re.abs() < std::f64::EPSILON || (z.re <= 0.0_f64 && z.im == 0.0_f64) {
+        Complex::new(0.0_f64, 0.0_f64)
     } else {
         let pi = std::f64::consts::PI;
-        let z3 = 1.202056903159594;
+        let z3 = 1.202056903159594_f64;
 
-        z.li3() + (1.-z).li3() + (1.-1./z).li3()
-            - (z3 + z.cln().powi(3)/6. + pi*pi/6.*z.cln() - 0.5*z.cln().powi(2)*(1.-z).cln())
+        z.li3() + (1.0_f64 - z).li3() + (1.0_f64 - 1.0_f64/z).li3()
+            - (z3 + z.cln().powi(3)/6.0_f64 + pi*pi/6.0_f64*z.cln()
+               - 0.5_f64*z.cln().powi(2)*(1.0_f64 - z).cln())
     }
 }
 
@@ -38,39 +38,39 @@ fn special_values() {
     use num::Zero;
     let pi  = std::f64::consts::PI;
     let pi2 = pi*pi;
-    let eps = 1e-15;
-    let ln2 = 2.0.ln();
-    let z3  = 1.202056903159594;
-    let phi = 0.5*((5.0).sqrt() + 1.0); // golden ratio
+    let eps = 1e-15_f64;
+    let ln2 = 2.0_f64.ln();
+    let z3  = 1.202056903159594_f64;
+    let phi = 0.5_f64*(5.0_f64.sqrt() + 1.0_f64); // golden ratio
     let zero = Complex::zero();
 
     assert_eq_complex!(zero.li3(), zero, eps);
-    assert_eq_complex!(Complex::new(1., 0.).li3(),
-                       Complex::new(z3, 0.), eps);
-    assert_eq_complex!(Complex::new(-1., 0.).li3(),
-                       Complex::new(-3./4.*z3, 0.), eps);
-    assert_eq_complex!(Complex::new(0.5, 0.).li3(),
-                       Complex::new(ln2.powi(3)/6. - pi2/12.*ln2 + 7./8.*z3, 0.), eps);
-    assert_eq_complex!(Complex::new(1./(phi*phi), 0.).li3(),
-                       Complex::new(4./5.*z3 + 2./3.*phi.ln().powi(3) - 2./15.*pi2*phi.ln(), 0.), eps);
+    assert_eq_complex!(Complex::new(1.0_f64, 0.0_f64).li3(),
+                       Complex::new(z3, 0.0_f64), eps);
+    assert_eq_complex!(Complex::new(-1.0_f64, 0.0_f64).li3(),
+                       Complex::new(-3.0_f64/4.0_f64*z3, 0.0_f64), eps);
+    assert_eq_complex!(Complex::new(0.5_f64, 0.0_f64).li3(),
+                       Complex::new(ln2.powi(3)/6.0_f64 - pi2/12.0_f64*ln2 + 7.0_f64/8.0_f64*z3, 0.0_f64), eps);
+    assert_eq_complex!(Complex::new(1.0_f64/(phi*phi), 0.0_f64).li3(),
+                       Complex::new(4.0_f64/5.0_f64*z3 + 2.0_f64/3.0_f64*phi.ln().powi(3) - 2.0_f64/15.0_f64*pi2*phi.ln(), 0.0_f64), eps);
 
     // test value that causes overflow if squared
-    assert!(!Complex::new(1e300, 1.0).li3().is_infinite());
-    assert!(!Complex::new(1.0, 1e300).li3().is_infinite());
-    assert_eq_complex!(Complex::new(1e300, 1.0).li3(), Complex::new(-5.4934049431527088e7, 749538.186928224), eps);
-    assert_eq_complex!(Complex::new(1.0, 1e300).li3(), Complex::new(-5.4936606061973454e7, 374771.031356405), eps);
+    assert!(!Complex::new(1e300_f64, 1.0_f64).li3().is_infinite());
+    assert!(!Complex::new(1.0_f64, 1e300_f64).li3().is_infinite());
+    assert_eq_complex!(Complex::new(1e300_f64, 1.0_f64).li3(), Complex::new(-5.4934049431527088e7_f64, 749538.186928224_f64), eps);
+    assert_eq_complex!(Complex::new(1.0_f64, 1e300_f64).li3(), Complex::new(-5.4936606061973454e7_f64, 374771.031356405_f64), eps);
 }
 
 
 #[test]
 fn test_values() {
-    let eps = 1e-14;
+    let eps = 1e-14_f64;
     let values = common::read_data_file("Li3.txt").unwrap();
 
     for &(v, li3) in values.iter() {
         assert_eq_complex!(v.li3(), li3, eps);
 
-        if v.im == 0.0 {
+        if v.im == 0.0_f64 {
             assert_eq_float!(v.re.li3(), li3.re, eps);
         }
     }
@@ -80,7 +80,7 @@ fn test_values() {
 #[test]
 fn identities() {
     use num::Zero;
-    let eps = 1e-9;
+    let eps = 1e-9_f64;
     let zero = Complex::<f64>::zero();
     let values = common::read_data_file("Li3.txt").unwrap();
 
