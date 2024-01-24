64d3d5b02dc2a0a10e01233854836a504ab60c40
Alexander Voigt <ahgvoigt@gmail.com>
2024-01-24 06:46:28 +0100

    use explicit f64 type instead of {float}

--- tests/li6.rs
+++ tests/li6.rs
@@ -1,5 +1,4 @@
 use num::complex::Complex;
-use num::Float;
 use polylog::Li6;
 mod common;
 
@@ -7,29 +6,29 @@ mod common;
 #[test]
 fn special_values() {
     use num::Zero;
-    let eps = 1e-15;
-    let z6  = 1.017343061984449;
+    let eps = 1e-15_f64;
+    let z6  = 1.017343061984449_f64;
     let zero = Complex::zero();
 
     assert_eq_complex!(zero.li6(), zero, eps);
-    assert_eq_complex!(Complex::<f64>::new(1., 0.).li6(),
-                       Complex::<f64>::new(z6, 0.), eps);
-    assert_eq_complex!(Complex::<f64>::new(-1., 0.).li6(),
-                       Complex::<f64>::new(-31./32.*z6, 0.), eps);
-    assert_eq_complex!(Complex::<f64>::new(0.5, 0.).li6(),
-                       Complex::<f64>::new(0.5040953978039886, 0.), eps);
+    assert_eq_complex!(Complex::<f64>::new(1.0_f64, 0.0_f64).li6(),
+                       Complex::<f64>::new(z6, 0.0_f64), eps);
+    assert_eq_complex!(Complex::<f64>::new(-1.0_f64, 0.0_f64).li6(),
+                       Complex::<f64>::new(-31.0_f64/32.0_f64*z6, 0.0_f64), eps);
+    assert_eq_complex!(Complex::<f64>::new(0.5_f64, 0.0_f64).li6(),
+                       Complex::<f64>::new(0.5040953978039886_f64, 0.0_f64), eps);
 
     // test value that causes overflow if squared
-    assert!(!Complex::new(1e300, 1.0).li6().is_infinite());
-    assert!(!Complex::new(1.0, 1e300).li6().is_infinite());
-    assert_eq_complex!(Complex::new(1e300, 1.0).li6(), Complex::new(-1.5086876165613597e14, 4.11768711823317e12), 2.0*eps);
-    assert_eq_complex!(Complex::new(1.0, 1e300).li6(), Complex::new(-1.5090387516918862e14, 2.0589500211678e12), 2.0*eps);
+    assert!(!Complex::new(1e300_f64, 1.0_f64).li6().is_infinite());
+    assert!(!Complex::new(1.0_f64, 1e300_f64).li6().is_infinite());
+    assert_eq_complex!(Complex::new(1e300_f64, 1.0_f64).li6(), Complex::new(-1.5086876165613597e14_f64, 4.11768711823317e12_f64), 2.0_f64*eps);
+    assert_eq_complex!(Complex::new(1.0_f64, 1e300_f64).li6(), Complex::new(-1.5090387516918862e14_f64, 2.0589500211678e12_f64), 2.0_f64*eps);
 }
 
 
 #[test]
 fn test_values() {
-    let eps = 1e-14;
+    let eps = 1e-14_f64;
     let values = common::read_data_file("Li6.txt").unwrap();
 
     for &(v, li6) in values.iter() {
