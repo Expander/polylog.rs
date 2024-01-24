8af8df4752a6f8a7d8f5dec2a4d61a6784b87d93
Alexander Voigt <ahgvoigt@gmail.com>
2024-01-24 06:49:05 +0100

    use explicit f64 type instead of {float}

--- tests/li0.rs
+++ tests/li0.rs
@@ -1,28 +1,27 @@
 use num::complex::Complex;
-use num::Float;
 use polylog::Li0;
 mod common;
 
 
 #[test]
 fn test_values() {
-    let eps = 1e-14;
+    let eps = 1e-14_f64;
     let values = common::read_data_file("Li0.txt").unwrap();
 
     for &(v, li0) in values.iter() {
         assert_eq_complex!(v.li0(), li0, eps);
 
-        if v.im == 0.0 {
+        if v.im == 0.0_f64 {
             assert_eq_float!(v.re.li0(), li0.re, eps);
         }
     }
 
-    assert!(1.0.li0().is_infinite());
-    assert!(Complex::new(1.0, 0.0).li0().is_nan());
+    assert!(1.0_f64.li0().is_infinite());
+    assert!(Complex::new(1.0_f64, 0.0_f64).li0().is_nan());
 
     // test value that causes overflow if squared
-    assert!(!Complex::new(1e300, 1.0).li0().is_infinite());
-    assert!(!Complex::new(1.0, 1e300).li0().is_infinite());
-    assert_eq_complex!(Complex::new(1e300, 1.0).li0(), Complex::new(-1.0, 0.0), eps);
-    assert_eq_complex!(Complex::new(1.0, 1e300).li0(), Complex::new(-1.0, 0.0), eps);
+    assert!(!Complex::new(1e300_f64, 1.0_f64).li0().is_infinite());
+    assert!(!Complex::new(1.0_f64, 1e300_f64).li0().is_infinite());
+    assert_eq_complex!(Complex::new(1e300_f64, 1.0_f64).li0(), Complex::new(-1.0_f64, 0.0_f64), eps);
+    assert_eq_complex!(Complex::new(1.0_f64, 1e300_f64).li0(), Complex::new(-1.0_f64, 0.0_f64), eps);
 }
--- tests/li1.rs
+++ tests/li1.rs
@@ -1,28 +1,27 @@
 use num::complex::Complex;
-use num::Float;
 use polylog::Li1;
 mod common;
 
 
 #[test]
 fn test_values() {
-    let eps = 1e-14;
+    let eps = 1e-14_f64;
     let values = common::read_data_file("Li1.txt").unwrap();
 
     for &(v, li1) in values.iter() {
         assert_eq_complex!(v.li1(), li1, eps);
 
-        if v.im == 0.0 {
+        if v.im == 0.0_f64 {
             assert_eq_float!(v.re.li1(), li1.re, eps);
         }
     }
 
-    assert!(1.0.li1().is_infinite());
-    assert!(Complex::new(1.0, 0.0).li1().is_infinite());
+    assert!(1.0_f64.li1().is_infinite());
+    assert!(Complex::new(1.0_f64, 0.0_f64).li1().is_infinite());
 
     // test value that causes overflow if squared
-    assert!(!Complex::new(1e300, 1.0).li1().is_infinite());
-    assert!(!Complex::new(1.0, 1e300).li1().is_infinite());
-    assert_eq_complex!(Complex::new(1e300, 1.0).li1(), Complex::new(-690.77552789821371, 3.14159265358979), eps);
-    assert_eq_complex!(Complex::new(1.0, 1e300).li1(), Complex::new(-690.77552789821371, 1.5707963267948966), eps);
+    assert!(!Complex::new(1e300_f64, 1.0_f64).li1().is_infinite());
+    assert!(!Complex::new(1.0_f64, 1e300_f64).li1().is_infinite());
+    assert_eq_complex!(Complex::new(1e300_f64, 1.0_f64).li1(), Complex::new(-690.77552789821371_f64, 3.14159265358979_f64), eps);
+    assert_eq_complex!(Complex::new(1.0_f64, 1e300_f64).li1(), Complex::new(-690.77552789821371_f64, 1.5707963267948966_f64), eps);
 }
