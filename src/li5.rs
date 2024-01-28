65d0f36157439c6fdaa50d60155aac7dd45aab93
Alexander Voigt <ahgvoigt@gmail.com>
2024-01-28 12:45:09 +0100

    avoid return statement

--- src/li5.rs
+++ src/li5.rs
@@ -35,38 +35,33 @@ impl Li5<Complex<f64>> for Complex<f64> {
             1.0903545401333394e-15
         ];
 
-        if self.im == 0.0 {
-            if self.re == 0.0 {
-                return Complex::new(0.0, 0.0);
-            }
-            if self.re == 1.0 {
-                return Complex::new(z5, 0.0);
-            }
-            if self.re == -1.0 {
-                return Complex::new(-15.0/16.0*z5, 0.0);
-            }
-        }
+        if self.im == 0.0 && self.re == 0.0 {
+            Complex::new(0.0, 0.0)
+        } else if self.im == 0.0 && self.re == 1.0 {
+            Complex::new(z5, 0.0)
+        } else if self.im == 0.0 && self.re == -1.0 {
+            Complex::new(-15.0/16.0*z5, 0.0)
+        } else {
+            let nz  = self.norm();
+            let pz  = self.arg();
+            let lnz = nz.ln();
 
-        let nz  = self.norm();
-        let pz  = self.arg();
-        let lnz = nz.ln();
+            if lnz*lnz + pz*pz < 1.0 { // |log(z)| < 1
+                let u  = Complex::new(lnz, pz);
+                let u2 = u*u;
+                let c1 = 1.0823232337111382; // zeta(4)
+                let c2 = 0.60102845157979714; // zeta(3)/2
+                let c3 = 0.27415567780803774;
+                let c4 = (25.0/12.0 - (-u).cln())/24.0;
+                let c5 = -1.0/240.0;
 
-        if lnz*lnz + pz*pz < 1.0 { // |log(z)| < 1
-            let u  = Complex::new(lnz, pz);
-            let u2 = u*u;
-            let c1 = 1.0823232337111382; // zeta(4)
-            let c2 = 0.60102845157979714; // zeta(3)/2
-            let c3 = 0.27415567780803774;
-            let c4 = (25.0/12.0 - (-u).cln())/24.0;
-            let c5 = -1.0/240.0;
+                let cs = [
+                    -1.1574074074074074e-04, 2.0667989417989418e-07,
+                    -1.0935444136502338e-09, 8.6986487449450412e-12,
+                    -8.6899587861588824e-14, 1.0081254080218813e-15
+                ];
 
-            let cs = [
-                -1.1574074074074074e-04, 2.0667989417989418e-07,
-                -1.0935444136502338e-09, 8.6986487449450412e-12,
-                -8.6899587861588824e-14, 1.0081254080218813e-15
-            ];
-
-            return z5 + u * c1 +
+                z5 + u * c1 +
                 u2 * (c2 + u * c3 +
                 u2 * (c4 + u * c5 +
                 u2 * (cs[0] +
@@ -74,29 +69,30 @@ impl Li5<Complex<f64>> for Complex<f64> {
                 u2 * (cs[2] +
                 u2 * (cs[3] +
                 u2 * (cs[4] +
-                u2 * (cs[5]))))))));
+                u2 * (cs[5]))))))))
+            } else {
+                let (u, rest) = if nz <= 1.0 {
+                    (-(1.0 - self).cln(), Complex::new(0.0, 0.0))
+                } else { // nz > 1.0
+                    let pi4  = pi2*pi2;
+                    let arg = if pz > 0.0 { pz - pi } else { pz + pi };
+                    let lmz = Complex::new(lnz, arg); // (-self).cln()
+                    let lmz2 = lmz*lmz;
+                    (-(1.0 - 1.0/self).cln(), -1.0/360.0*lmz*(7.0*pi4 + lmz2*(10.0*pi2 + 3.0*lmz2)))
+                };
+
+                let u2 = u*u;
+                let u4 = u2*u2;
+                let u8 = u4*u4;
+
+                rest +
+                u*bf[0] +
+                u2*(bf[1] + u*bf[2]) +
+                u4*(bf[3] + u*bf[4] + u2*(bf[5] + u*bf[6])) +
+                u8*(bf[7] + u*bf[8] + u2*(bf[9] + u*bf[10]) +
+                    u4*(bf[11] + u*bf[12] + u2*(bf[13] + u*bf[14]))) +
+                u8*u8*(bf[15] + u*bf[16] + u2*(bf[17] + u*bf[18]))
+            }
         }
-
-        let (u, rest) = if nz <= 1.0 {
-            (-(1.0 - self).cln(), Complex::new(0.0, 0.0))
-        } else { // nz > 1.0
-            let pi4  = pi2*pi2;
-            let arg = if pz > 0.0 { pz - pi } else { pz + pi };
-            let lmz = Complex::new(lnz, arg); // (-self).cln()
-            let lmz2 = lmz*lmz;
-            (-(1.0 - 1.0/self).cln(), -1.0/360.0*lmz*(7.0*pi4 + lmz2*(10.0*pi2 + 3.0*lmz2)))
-        };
-
-        let u2 = u*u;
-        let u4 = u2*u2;
-        let u8 = u4*u4;
-
-        rest +
-        u*bf[0] +
-        u2*(bf[1] + u*bf[2]) +
-        u4*(bf[3] + u*bf[4] + u2*(bf[5] + u*bf[6])) +
-        u8*(bf[7] + u*bf[8] + u2*(bf[9] + u*bf[10]) +
-            u4*(bf[11] + u*bf[12] + u2*(bf[13] + u*bf[14]))) +
-        u8*u8*(bf[15] + u*bf[16] + u2*(bf[17] + u*bf[18]))
     }
 }
