193e1846b5fdea0322ab6107b3c4c3ec1b64e06e
Alexander Voigt <alexander.voigt@physik.rwth-aachen.de>
2024-03-06 13:01:27 +0100

    handle signed zero in li1

--- tests/li1.rs
+++ tests/li1.rs
@@ -25,3 +25,22 @@ fn test_values() {
     assert_eq_complex!(Complex::new(1e300_f64, 1.0_f64).li1(), Complex::new(-690.77552789821371_f64, 3.14159265358979_f64), eps);
     assert_eq_complex!(Complex::new(1.0_f64, 1e300_f64).li1(), Complex::new(-690.77552789821371_f64, 1.5707963267948966_f64), eps);
 }
+
+
+#[test]
+fn test_signed_zero() {
+    let pz64 = 0.0_f64;
+    let nz64 = -0.0_f64;
+
+    assert!(pz64.li1().is_sign_positive());
+    assert!(nz64.li1().is_sign_negative());
+
+    assert!(Complex::new(pz64, pz64).li1().re.is_sign_positive());
+    assert!(Complex::new(pz64, pz64).li1().im.is_sign_positive());
+    assert!(Complex::new(pz64, nz64).li1().re.is_sign_positive());
+    assert!(Complex::new(pz64, nz64).li1().im.is_sign_negative());
+    assert!(Complex::new(nz64, pz64).li1().re.is_sign_negative());
+    assert!(Complex::new(nz64, pz64).li1().im.is_sign_positive());
+    assert!(Complex::new(nz64, nz64).li1().re.is_sign_negative());
+    assert!(Complex::new(nz64, nz64).li1().im.is_sign_negative());
+}
